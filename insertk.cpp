c315b9c28cafe7853bc9bd7c1a5652651435002f
DEEPTI KULSHRESTHA <110666921+deeptikulshrestha@users.noreply.github.com>
2025-07-23 04:57:13 +0000

    insert at k

---
+++ insertk.cpp
@@ -0,0 +1,118 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+class Node{
+    public :
+    int data;
+    Node* next;
+    Node(int val) {
+        data = val;
+        next = nullptr;
+    }
+     Node(int val, Node* nextNode) { // << Add this
+        data = val;
+        next = nextNode;
+    }
+};
+
+Node* convertArr2LL(vector<int> &arr){
+    Node* head = new Node(arr[0]);
+    Node* mover = head;
+
+    for(int i=1;i<arr.size();i++){
+        Node* temp = new Node(arr[i]);
+        mover -> next = temp;
+        mover = temp;
+    }
+    return head;
+}
+void printLinkedList(Node* head) {
+    Node* temp = head;
+    while(temp != NULL) {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+Node* deleteTail(Node* head){
+    if(head==NULL || head->next == NULL) return NULL;
+    Node* temp = head;
+    while(temp->next->next != NULL){
+        temp = temp->next;
+    }
+    free(temp->next);
+    temp->next =nullptr;
+    return head;
+}
+
+Node* deleteEl(Node* head, int el){
+    if(head==NULL) return head;
+    if(head->data ==el){
+        Node* temp = head;
+        head = head->next;
+        free(temp);
+        return head;
+    }
+    Node* temp = head;
+    Node* prev = NULL;
+    while(temp!=NULL){
+        if(temp->data == el){
+            prev->next = prev->next->next;
+            free(temp);
+            break;
+        }
+         prev = temp;
+    temp = temp->next;
+    }
+    return head;
+}
+
+Node* insertHead(Node* head, int val){
+    Node* temp = new Node(val, head);
+    return temp;
+}
+
+Node* insertTail(Node* head,int val){
+    if(head==NULL){
+        return new Node(val);
+    }
+    Node* temp = head;
+    while(temp->next !=NULL){
+        temp=temp->next;
+    }
+
+    Node* newNode = new Node(val);
+    temp->next = newNode;
+    return head;
+}
+
+Node* insertk(Node* head,int el,int k){
+    if(head==NULL){
+        if(k==1) return new Node(el);
+        else return head;
+    }
+    if(k==1){
+        Node* temp = new Node(el,head);
+        return temp;
+    }
+    int count = 0;
+    Node* temp = head;
+    while(temp!=NULL){
+        count++;
+
+        if(count == (k-1)){
+            Node* x = new Node(el);
+            x->next = temp->next;
+            temp->next = x;
+            break;
+        }
+        temp = temp->next;
+    }
+    return head;
+}
+int main(){
+    vector<int> arr = {12,13,4,5,6};
+    Node* head = convertArr2LL(arr);
+    head = insertk(head,8,4);
+    printLinkedList(head);
+}
